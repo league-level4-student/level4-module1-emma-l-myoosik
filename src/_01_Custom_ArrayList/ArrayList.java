1e3f0470d930c503cec502c3b78d03167df21562
emma-l-myoosik <60460092+emma-l-myoosik@users.noreply.github.com>
2020-07-21 13:30:19 -0700

    arraylist

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -3,21 +3,32 @@ package _01_Custom_ArrayList;
 @SuppressWarnings("unchecked")
 
 public class ArrayList <T>{
+	private T arr[];
 	
 	public ArrayList() {
+		arr = (T[]) new Object[0];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return arr[loc];
 	}
 	
 	public void add(T val) {
+		T arr2[] = (T[]) new Object[arr.length + 1];
+		arr2[arr2.length - 1] = val;
 		
+		for (int i = 0; i < arr.length; i++) {
+			arr2[i] = arr[i];
+		}
+		
+		arr = arr2;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T arr2[] = (T[]) new Object[arr.length + 1];
+		for (int i = 0; i < arr2.length; i++) {
+			
+		}
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
@@ -31,5 +42,9 @@ public class ArrayList <T>{
 	public boolean contains(T val) {
 		
 		return false;
+	}
+	
+	public int size() {
+		return arr.length;
 	}
 }
